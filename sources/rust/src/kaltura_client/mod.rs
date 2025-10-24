e5a95df612c7177259c44ed5e9287fa4d497d2b1
Ben Raz <ben.raz2008@gmail.com>
2025-10-24 11:34:41 -0400

    added module level documentation to 'classes'
    Typically, there are two ways to add documentation to a module.
    
    The first method (which is more idiomatic) is two write a top-level
    documentation block at the top of the module source file as such:
    
    a/b/c.rs:
        //! doc for module c
    
    The other method is to adorn a `mod` item with a documentation comment,
    as such:
    
    a/b.rs
        /// doc for module c
        mod c;
    
    The reason I preffered to go for the second option is that the first
    option is impossible due to the classes.rs file being overwritten. I
    could have hardcoded the documentation into the
    `lib/RustClientGenerator` script but that would have been a considerable
    worse solution.
    
    Signed-off-by: Ben Raz <ben.raz2008@gmail.com>

--- sources/rust/src/kaltura_client/mod.rs
+++ sources/rust/src/kaltura_client/mod.rs
@@ -1,3 +1,37 @@
 pub mod enums;
+
+/// Classes provided as part of the [Kaltura API Client Library](https://developer.kaltura.com/api-docs/General_Objects/Objects)
+///
+/// ## Architecture
+///
+/// Because the Kaltura API Client is object oriented, certain abstractions have been chosen to
+/// approximate inheritance.
+///
+/// Assuming there is a class `KalturaC`, which inherits from `KalturaB`, which inherits from
+/// `KalturaA`, this relationship would be expressed as such:
+///
+/// ```rust
+/// struct KalturaA;
+///
+/// trait IKalturaA {};
+///
+/// struct KalturaB;
+/// impl IKalturaA for KalturaB {}
+/// trait IKalturaB: IKalturaA {}
+///
+/// struct KalturaC;
+///
+/// impl IKalturaA for KalturaC {}
+/// impl IKalturaB for KalturaC {}
+///
+/// fn use_a(x: impl KalturaA) {}
+/// fn use_b(x: impl KalturaB) {}
+/// # fn main() {
+///
+/// let c = KalturaC;
+/// use_a(c);
+/// use_b(c);
+/// # }
+/// ```
 pub mod classes;
 pub mod traits;
